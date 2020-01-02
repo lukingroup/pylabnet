4b316abb1d88fb4f837dff5d510b74915dc1498a
Roger <b16lukin@gmail.com>
2020-01-02 16:27:43 -0500

    Slight changes to NiDaqMxCard

--- pylabnet/hardware/ni_daqs/nidaqmx_card.py
+++ pylabnet/hardware/ni_daqs/nidaqmx_card.py
@@ -7,11 +7,13 @@ This file contains the pylabnet Hardware module class for a generic NI DAQ mx ca
 import nidaqmx
 from pylabnet.utils.logging.logger import LogHandler
 
-class NiDaqMxCard():
+
+class NiDaqMxCard:
 
     def __init__(self, device_name, logger=None):
         """Instantiate NI DAQ mx card
-        :device_name: (string) Name of NI DAQ mx card, as displayed in the measurement and automation explorer
+
+        :device_name: (str) Name of NI DAQ mx card, as displayed in the measurement and automation explorer
         """
 
         # Device name
@@ -20,35 +22,62 @@ class NiDaqMxCard():
         # Log
         self.log = LogHandler(logger=logger)
 
-        # Try to get infos of DAQ device to verify connection
+        # Try to get info of DAQ device to verify connection
         try:
             ni_daq_device = nidaqmx.system.device.Device(name=device_name)
             self.log.info(
-                "Successfully connected to NI DAQ '{device_name}' (type: {product_type}) \n".format\
-                    (device_name=ni_daq_device.name, product_type=ni_daq_device.product_type)
+                "Successfully connected to NI DAQ '{device_name}' (type: {product_type}) \n"
+                "".format(
+                    device_name=ni_daq_device.name,
+                    product_type=ni_daq_device.product_type
                 )
+            )
 
         # If failed, provide info about connected DAQs
-        except nidaqmx.DaqError:
-            # Get names of all connected NI DAQs
+        except nidaqmx.DaqError as exc_obj:
+
+            # Log exception message
+
+            # - get names of all connected NI DAQs
             ni_daqs_names = nidaqmx.system._collections.device_collection.\
                 DeviceCollection().device_names
-            self.log.error('NI DAQ {device_name} not found: \n'.format(device_name=device_name))
-            self.log.error('Available {} NI DAQs: \n'.format(len(ni_daqs_names)))
-            self.log.error('{}  \n'.format(ni_daqs_names))
 
-    def get_channel_path(self, channel):
-        return"{device_name}/{channel}".format\
-            (device_name=self.dev, channel=channel)
+            # - exception message
+            self.log.exception(
+                "NI DAQ card {} not found. \n"
+                "There are {} NI DAQs available: \n "
+                "    {}"
+                "".format(
+                    device_name,
+                    len(ni_daqs_names),
+                    ni_daqs_names
+                )
+            )
+
+            # Raise exception
+            raise exc_obj
+
+    def _gen_ch_path(self, channel):
+        """ Auxiliary method to build channel path string.
+
+        :param channel: (str) channel name ['ao1']
+        :return: (str) full channel name ['Dev1/ao1']
+        """
+
+        return "{device_name}/{channel}".format(
+            device_name=self.dev,
+            channel=channel
+        )
 
     def set_ao_voltage(self, ao_channel, voltages):
-        """Set analog output of NI DAQ mx card to series of voltages
-        :ao_channel: (string) Name of output channel (e.g. 'ao1', 'ao2')
-        :voltages: (list of integers) list of volateges which will be output
+        """Set analog output of NI DAQ mx card to a series of voltages
+
+        :ao_channel: (str) Name of output channel (e.g. 'ao1', 'ao2')
+        :voltages: (list of int) list of voltages which will be output
         """
 
         # TODO: Understand the timing between ouput voltages (sample-wise?)
-        channel = self.get_channel_path(ao_channel)
+        channel = self._gen_ch_path(ao_channel)
 
         with nidaqmx.Task() as task:
             task.ao_channels.add_ao_voltage_chan(channel)

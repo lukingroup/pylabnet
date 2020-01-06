f2f679abf4627f025957f5d694b331cd2aa10535
mkbhaskar <mbhaskar@g.harvard.edu>
2020-01-06 17:43:15 -0500

    Changed to dictionary based data storage. Implemented user control of display refresh rate and maximized wavemeter sampling rate for (future) locking purposes

--- pylabnet/scripts/wlm_monitor.py
+++ pylabnet/scripts/wlm_monitor.py
@@ -15,71 +15,75 @@ class WlmMonitor:
 
         self._wlm = wlm_client
 
+        # self._is_running = False
+        # self._nd_trace = None
+        # self._ch_list = [1]
+        # self._num_ch = len(self._ch_list)
+        # self._ch_names = ['Channel 1']
+        # self._x_axis = np.arange(100)
+        # self._length = len(self._x_axis)
+        # self._y_axis = np.zeros((1,self._length))
+        # self._s_axis = None
+        # self._setpoints = [None]
+        # self._locks = [False]
+        
         self._is_running = False
-        self._nd_trace = None
-        self._ch_list = [1]
-        self._num_ch = len(self._ch_list)
-        self._ch_names = ['Channel 1']
-        self._x_axis = np.arange(100)
-        self._length = len(self._x_axis)
-        self._y_axis = np.zeros((1,self._length))
-        self._s_axis = None
-        self._setpoints = [None]
-        self._locks = [False]
+        self.channels = [None]
+        self._x_axis = None
+        self._length = 0
+        self._update_rate = 0
 
-    def set_params(self, ch_list=[1], setpoints=[None], locks=[False], display_pts=1000, units="Frequency (THz)"):
+    def set_params(self, channels=None, update_rate=0.05, display_pts=1000):
         """
         Configures script parameters for wavemeter control
-
-        :param ch_list: array of wavemeter channels to log from 1-8
-        :param setpoints: laser freqeuncy setpoints
-        :param locks: whether or not to lock the laser
-        :param display_pts: number of samples to display on plot
-        :param units: string describing units of wavemeter output
+        
+        :param channels: list of dictionaries containing channel information in the example structure:
+            {"channel": 1, "setpoint": None, "lock": False}
+        :param update_rate: how often to update the plot in seconds
+            Note: this should be increased in order to avoid lagging in plot outputting
+        :param display_pts: number of points to display on graph
         """
-
-        self._ch_list = ch_list
-
-        # Properly initialize setpoints if not provided
-        if len(setpoints) == len(ch_list):
-            self._setpoints = setpoints
-        else:
-            self._setpoints = []
-            for channel in ch_list:
-                self._setpoints.extend(None)
-
-        self._locks = locks
+        
         self._length = display_pts
-        self._num_ch = len(self._ch_list)
         self._x_axis = np.arange(self._length)
-        self._y_axis = np.zeros((self._num_ch, self._length))
-        self._s_axis = np.zeros((self._num_ch, self._length))
-
-        # Initialize GUI
-        self._nd_trace = []
-        for index, channel in enumerate(self._ch_list):
-
-            # If a setpoint is given, prepare to plot it
-            if self._setpoints[index] is not None:
-                self._nd_trace.append(MultiTraceFig(
-                    title_str="Channel {} Laser monitor".format(channel),
-                    ch_names=["Channel {}".format(channel), "Channel {} SP".format(channel)],
-                    shot_noise=False
-                ))
-
-            # Otherwise just a single plot for each channel
+        self._update_rate = update_rate
+        
+        # Initialize to default settings if nothing provided
+        if channels is None:
+            self.channels = [{
+                "channel": 1, 
+                "setpoint": None, 
+                "lock": False
+            }]
+        else:
+            self.channels = channels
+                
+        for channel in self.channels:
+            # Add placeholder for data
+            channel["data"] = np.zeros(self._length)
+            
+            # We need to configure the plot differently based on whether or not a setpoint is given
+            if channel["setpoint"] is None:
+                channel["sp_data"] = None
+                
+                # Initialize plot with no setpoints
+                channel["trace"] = MultiTraceFig(title_str="Channel {} Laser Monitor".format(channel["channel"]))
+                
             else:
-                self._nd_trace.append(MultiTraceFig(
-                    title_str="Channel {} Laser monitor".format(channel),
-                    ch_names=["Channel {}".format(channel)],
-                    shot_noise=False
-                ))
-
-        for trace in self._nd_trace:
-            trace.set_lbls(
+                channel["sp_data"] = np.ones(self._length)*channel["setpoint"]
+                
+                # Initialize plot with setpoints
+                channel["trace"] = MultiTraceFig(
+                    title_str="Channel {} Laser Monitor".format(channel["channel"]),
+                    ch_names=["Channel {} Frequency".format(channel["channel"]),
+                              "Channel {} Setpoint".format(channel["channel"])]
+                )
+            
+            # Set labels
+            channel["trace"].set_lbls(
                 x_str="Time (pts)",
-                y_str=units
-            )
+                y_str="Frequency (THz)"
+                )
 
     def run(self):
         """Runs the wavemeter monitor"""
@@ -95,10 +99,14 @@ class WlmMonitor:
 
             # Continuously update data until paused
             while self._is_running:
+                start_time = time.time()
+                while time.time() - start_time < self._update_rate:
+                    self._update_data()
+                    time.sleep(0.002)
                 self._update_output()
 
                 # Pause to give time for update
-                time.sleep(0.05)
+                # time.sleep(0.05)
 
         except Exception as exc_obj:
             self._is_running = False
@@ -110,62 +118,69 @@ class WlmMonitor:
         self._is_running = False
 
     # Technical methods
-
+    
     def _initialize_display(self):
         """Initializes the display, sets all output points to the first reading for each WL"""
-
-        for index, channel in enumerate(self._ch_list):
+        
+        for channel in self.channels:
 
             # Get current wavelength
-            wavelength = self._wlm.get_wavelength(channel)
-
-            # Generate constant array with current wavelength
-            self._y_axis[index] = np.ones(self._length)*wavelength
-
-            # Set data
-            self._nd_trace[index].set_data(
+            wavelength = self._wlm.get_wavelength(channel["channel"])
+            
+            # Generate constant array with current wavelength for initialization
+            channel["data"] = np.ones(self._length)*wavelength
+            
+            # Set the data to the plot
+            channel["trace"].set_data(
                 x_ar=self._x_axis,
-                y_ar=self._y_axis[index]
+                y_ar=channel["data"]
             )
-
-            # Plot the setpoint as well if provided
-            if self._setpoints[index] is not None:
-                self._s_axis[index] = np.ones(self._length)*self._setpoints[index]
-                self._nd_trace[index].set_data(
+            
+            # Plot the setpoint as well if it exists
+            if channel["setpoint"] is not None:
+                channel["sp_data"] = np.ones(self._length)*channel["setpoint"]
+                channel["trace"].set_data(
                     x_ar=self._x_axis,
-                    y_ar=self._s_axis[index],
-                    ind=1
+                    y_ar=channel["sp_data"]
                 )
-
+            
             # Show graph
-            self._nd_trace[index].show()
+            channel["trace"].show()
+
+    def _update_data(self):
+        """Pulls a new sample from the WLM into the data for each channel"""
+
+        for channel in self.channels:
+            # Get current wavelength
+            wavelength = self._wlm.get_wavelength(channel["channel"])
+
+            # Update output array
+            channel["data"] = np.append(channel["data"][1:], wavelength)
+
+            # Update the setpoint if needed
+            if channel["setpoint"] is not None:
+
+                # Add the current setpoint to the data (in case it has changed)
+                channel["sp_data"] = np.append(channel["sp_data"][1:], channel["setpoint"])
 
     def _update_output(self):
         """Updates the output with a new wavemeter sample"""
 
-        for index, channel in enumerate(self._ch_list):
-            # Get current wavelength
-            wavelength = self._wlm.get_wavelength(channel)
-
-            # Update output array
-            self._y_axis[index] = np.append(self._y_axis[index][1:], wavelength)
+        for channel in self.channels:
 
             # Set data
-            self._nd_trace[index].set_data(
+            channel["trace"].set_data(
                 x_ar=self._x_axis,
-                y_ar=self._y_axis[index]
+                y_ar=channel["data"]
             )
 
             # Plot the setpoint as well if provided
-            if self._setpoints[index] is not None:
-
-                # Add the current setpoint to the data (in case it has changed)
-                # self._s_axis[index] = np.append(self._s_axis[index][1:], self._setpoints[index])
+            if channel["setpoint"] is not None:
 
                 # Set data
-                self._nd_trace[index].set_data(
+                channel["trace"].set_data(
                     x_ar=self._x_axis,
-                    y_ar=self._s_axis[index],
+                    y_ar=channel["sp_data"],
                     ind=1
                 )
 

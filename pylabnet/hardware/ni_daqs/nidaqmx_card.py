7c692074893fdb3b7e9c6ff2d6dec903a4da2543
mkbhaskar <mbhaskar@g.harvard.edu>
2020-01-28 21:49:46 -0500

    started to modify nidaqmx_card.py driver to add counter functionality. still incomplete

--- pylabnet/hardware/ni_daqs/nidaqmx_card.py
+++ pylabnet/hardware/ni_daqs/nidaqmx_card.py
@@ -5,6 +5,7 @@ This file contains the pylabnet Hardware module class for a generic NI DAQ mx ca
 """
 
 import nidaqmx
+from pylabnet.hardware.interface.gated_ctr import GatedCtrInterface
 
 from pylabnet.utils.logging.logger import LogHandler
 from pylabnet.core.service_base import ServiceBase
@@ -15,6 +16,7 @@ import pickle
 
 class Driver:
     """Driver for NI DAQmx card. Currently only implements setting AO voltage"""
+    # TODO implement counter
 
     def __init__(self, device_name, logger=None):
         """Instantiate NI DAQ mx card
@@ -110,3 +112,80 @@ class Client(ClientBase):
             ao_channel=ao_channel,
             voltage_pickle=voltage_pickle
         )
+
+
+class GatedCounter(GatedCtrInterface):
+    """ Hardware class for NI gated counter """
+
+    def __init__(self):
+        self.ci_channel = None
+        self.task = None
+        self.duration = 0.1
+        self._status = 'Inactive'
+
+    def activate_interface(self, counter_channel='Dev1/ctr0', physical_channel='Dev1/PFI0'):
+        """ Activates counter interface (creates a task, does not start it)
+
+        :param counter_channel: (str) channel of counter to use, e.g. 'Dev1/ctr0'
+        :param physical_channel: (str) channel of physical counter input, e.g. 'Dev1/PFI0'
+        """
+
+        # Create a task - note we have to be careful and close the task if something goes wrong
+        self.task = nidaqmx.Task()
+
+        try:
+            self.ci_channel = self.task.ci_channels.add_ci_count_edges_chan(counter_channel)
+            self.ci_channel.ci_count_edges_term = physical_channel
+            self._status = 'Active, but not counting'
+
+        except nidaqmx.DaqError:
+            self.task.close()
+            self._status = 'Inactive'
+            raise
+
+    def init_ctr(self, bin_number=0.1, gate_type=None):
+        """ Initializes gated counter parameters
+
+        :param bin_number: (double) number of seconds to read for
+        :param gate_type: (not used)
+        """
+        self.duration = bin_number
+        pass
+
+    def close_ctr(self):
+        """ Stops the task and closes it.
+
+        The interface must be reactivated using activate_interface command in order to resume counting """
+
+        try:
+            self.task.stop()
+        except nidaqmx.DaqError:
+            pass
+        self._status = 'Inactive'
+        self.task.close()
+
+    def start_counting(self):
+        """ Starts the counter """
+
+        self._status = 'Counting'
+        try:
+            self.task.start()
+            self.task.read()
+
+        except nidaqmx.DaqError:
+            self.task.close()
+            self._status = 'Inactive'
+
+    def terminate_counting(self):
+        pass
+
+    def get_status(self):
+        """ Returns status of the counter
+
+        :return: (str) whether or not the counter is active, and counting
+        """
+
+        return self._status
+
+    def get_count_ar(self, timeout=-1):
+        pass

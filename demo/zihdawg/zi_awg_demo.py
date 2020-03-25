8c8ba8b0a6c358dc20fbbe3795fc414c47eef211
cknaut <can.knaut@gmail.com>
2020-03-24 22:52:21 -0400

    Implemented awgModule setup function and sequence upload function.

--- demo/zihdawg/zi_awg_demo.py
+++ demo/zihdawg/zi_awg_demo.py
@@ -7,6 +7,8 @@ This module demonstrates the use of the ZI HDAWG Hardware Class.
 
 from pylabnet.hardware.zi_hdawg.zi_hdawg import HDAWG_Driver
 from pylabnet.utils.logging.logger import LogClient
+import textwrap
+
 
 
 dev_id = 'dev8040'
@@ -26,9 +28,39 @@ hd.set_channel_grouping(0)
 
 
 outputs = [0, 1]
-hd.enable_output(outputs)
 
+# Enable the outputs and set corresponding ranges
+hd.enable_output(outputs)
 for output in outputs:
     hd.set_output_range(output, 0.2)
+
+
+AWG_N = 16000
+sequence = textwrap.dedent("""\
+        const AWG_N = _c1_;
+        wave x = zeros(AWG_N);
+        wave y = zeros(AWG_N);
+
+        while(1){
+            setTrigger(1);
+            setTrigger(0);
+            playWave(1, x);
+            playWave(2, y);
+        }
+        """)
+
+# Fill in the integer constant AWG_N
+sequence = sequence.replace('_c1_', str(AWG_N))
+
+
+hd.set_channel_grouping(1)
+
+# Create an instance of the AWG Module
+
+awgModule = hd.setup_awg_module(1)
+
+if awgModule is not None:
+    hd.compile_upload_sequence(awgModule, sequence)
+
 
 hd.disable_everything()
--- pylabnet/hardware/ZI_HDAWG/zi_hdawg.py
+++ pylabnet/hardware/ZI_HDAWG/zi_hdawg.py
@@ -124,6 +124,7 @@ class HDAWG_Driver():
             2 : 1x8 with HDAWG8.
         """
         self.seti('system/awg/channelgrouping', index)
+        time.sleep(2)
 
     # Functions related to wave outputs:
 
@@ -198,3 +199,74 @@ class HDAWG_Driver():
                 self.log.error(f"Range {output_range} is not valid, allowed values for range are {allowed_ranges}")
         else:
             self.log.error(f"This device has only {self.num_outputs} channels, channel index {output_index} is invalid.")
+
+    def setup_awg_module(self, index):
+        """ Startup helper for awg module
+
+        :index: Which AWG sequencer to be used
+            0 - 3 for 4x2 channel grouping
+            0 - 1 for 2x4 channel grouping
+            0     for 1x8 channel grouping
+
+        Returns instance ow awgModule
+        """
+
+        # Check if chosen index is allowed for current channel grouping.
+        channel_grouping = self.geti('system/awg/channelgrouping')
+
+        if channel_grouping == 0:
+            num_awgs = 4
+        elif channel_grouping == 1:
+            num_awgs = 2
+        elif channel_grouping == 2:
+            num_awgs = 1
+
+        allowed_indices = range(num_awgs)
+
+        if index not in allowed_indices:
+            self.log.error(f"Current channel grouping only allows for the following AWG indices {list(allowed_indices)}")
+            return None
+
+        # Create an instance of the AWG Module
+        awgModule = self.daq.awgModule()
+        awgModule.set('index', index)
+        awgModule.set('device', self.device_id)
+        awgModule.execute()
+
+        return awgModule
+
+    def compile_upload_sequence(self, awgModule, sequence):
+        """ Compile and upload AWG sequence to AWG Module
+
+        :awgModule: Instance of awgModule.
+        :sequence: Valid .seqc sequence to be uploaded.
+        """
+
+        awgModule.set('compiler/sourcestring', sequence)
+        # Note: when using an AWG program from a source file (and only then), the compiler needs to
+        # be started explicitly with awgModule.set('compiler/start', 1)
+        while awgModule.getInt('compiler/status') == -1:
+            time.sleep(0.1)
+
+        if awgModule.getInt('compiler/status') == 1:
+            # compilation failed, raise an exception
+            self.log.warning(awgModule.getString('compiler/statusstring'))
+
+        if awgModule.getInt('compiler/status') == 0:
+            self.log.info("Compilation successful with no warnings, will upload the program to the instrument.")
+        if awgModule.getInt('compiler/status') == 2:
+            self.log.warning("Compilation successful with warnings, will upload the program to the instrument.")
+            self.log.warning("Compiler warning: ", awgModule.getString('compiler/statusstring'))
+
+        # Wait for the waveform upload to finish
+        time.sleep(0.2)
+        i = 0
+        while (awgModule.getDouble('progress') < 1.0) and (awgModule.getInt('elf/status') != 1):
+            self.log.info("{} progress: {:.2f}".format(i, awgModule.getDouble('progress')))
+            time.sleep(0.2)
+            i += 1
+        self.log.info("{} progress: {:.2f}".format(i, awgModule.getDouble('progress')))
+        if awgModule.getInt('elf/status') == 0:
+            self.log.info("Upload to the instrument successful.")
+        if awgModule.getInt('elf/status') == 1:
+            self.log.warning("Upload to the instrument failed.")

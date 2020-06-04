65faaf7da80275db12074517385cb855f7b73b5c
Rivka <rivka>
2020-06-03 21:07:56 -0400

    Added validation of connection

--- pylabnet/hardware/nanopositioners/smartact.py
+++ pylabnet/hardware/nanopositioners/smartact.py
@@ -25,21 +25,31 @@ class Nanopositioners():
         buffersize = ctypes.c_size_t(ctypes.sizeof(buffer)) #size _t gives c_ulonglong, not as in manual
         result = self._nanopositionersdll.SA_CTL_FindDevices(None, buffer,buffersize)
         buffervalue = buffer.value.decode("utf-8")
-        print('device found: '+ buffervalue)  #for debugging with breakpoint her
+        print('Device found at: '+ buffervalue)  #for debugging with breakpoint her
 
         #Establishes a connection to a device
         dhandle =  ctypes.c_uint32()
         connect = self._nanopositionersdll.SA_CTL_Open(ctypes.byref(dhandle), ctypes.byref(buffer), None)
-        print(str(dhandle.value))
+        #print('device dhanlde: ' + str(dhandle.value))
+        if connect == 0:
+            print('Success: device is connected')
 
         #Terminates connection to device
         self._nanopositionersdll.SA_CTL_Close.argtype = ctypes.POINTER(ctypes.c_uint32)
         self._nanopositionersdll.SA_CTL_Close.restype = ctypes.POINTER(ctypes.c_uint32)
         connect = self._nanopositionersdll.SA_CTL_Close(dhandle)
-        print(str(connect))
+        print('connect result: ' + str(connect))
 
 
+    #def Move(self)
 
+        #defines arguments and results for c functions
+        #self._nanopositionersdll.SA_CTL_SetProperty_i32 = [ctypes.POINTER(ctypes.c_uint32),ctypes.POINTER(ctypes.c_uint8),ctypes.POINTER(ctypes.c_uint32),ctypes.POINTER(ctypes.c_uint32)]
+        #self._nanopositionersdll.SA_CTL_Move.argtypes = [ctypes.POINTER(ctypes.c_uint32), ctypes.POINTER(ctypes.c_uint8), ctypes.POINTER(ctypes.c_uint64),ctypes.POINTER(ctypes.c_uint32)]
+        #self._nanopositionersdll.SA_CTL_Stop.argtypes = [ctypes.POINTER(ctypes.c_uint32), ctypes.POINTER(ctypes.c_uint8), ctypes.POINTER(ctypes.c_uint32)]
+
+        #idx = ctypes.c_uint32()
+        #self._nanopositionersdll.SA_CTL_Move(ctypes.byref(dhandle), ctypes.byref(idx), ctypes.byref(),   )
 
 def main():
     nanpos = Nanopositioners()

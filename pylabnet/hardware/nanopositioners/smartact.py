7d30c3e3bede14a6d4b3ed9ff696590f34483f6a
Rivka <rivka>
2020-06-16 17:17:37 -0400

    implemented move mode setting

--- pylabnet/hardware/nanopositioners/smartact.py
+++ pylabnet/hardware/nanopositioners/smartact.py
@@ -5,11 +5,13 @@ from pylabnet.utils.logging.logger import LogHandler
 
 class Nanopositioners():
 
-    STEP_MODE = ctypes.c_uint32(int("0x03050087",16))
-    PKEY_NUM_BUS = ctypes.c_uint32(int("0x020F0016",16))
-    PKEY_NUM_CH = ctypes.c_uint(int('0x020F0017', 16))
-    PKEY_NUM_BUS_CH = ctypes.c_uint(int('0x02030017', 16))
-    PKEY_MOVE_MODE = ctypes.c_uint(int('0x03050087', 16))
+    # property keys and other constants
+    PKEY_NUM_BUS = int("0x020F0016",16)
+    PKEY_NUM_CH = int('0x020F0017', 16)
+    PKEY_NUM_BUS_CH = int('0x02030017', 16)
+    PKEY_MOVE_MODE = int('0x03050087', 16)
+    MOVE_MODE_STEP = 4
+    MOVE_MODE_DC = 3
 
     def __init__(self, logger=None):
         """ Instantiate Nanopositioners"""
@@ -82,7 +84,7 @@ class Nanopositioners():
         freq = ctypes.c_int32(100)
         amp = ctypes.c_int32(100)
 
-        #set mode to relative movement and define frequency and amplitude
+        # set mode to relative movement and define frequency and amplitude
         resultmode = self._nanopositionersdll.SA_CTL_SetProperty_i32(self.dhandle, channel, modetype, stepmode) #define move mode SA_CTL_MOVE_MODE_STEP
         resultfreq = self._nanopositionersdll.SA_CTL_SetProperty_i32(self.dhandle, channel, profreq, freq)
         resultamp = self._nanopositionersdll.SA_CTL_SetProperty_i32(self.dhandle, channel, proamp,  amp)
@@ -92,21 +94,41 @@ class Nanopositioners():
         #if moveresult == 0:
         #    print('Positioner moving in channel ' + str(channel))
 
-    # def set_parameters(self, channel, mode=None, frequency=None, amplitude=None):
-    #     """ Sets parameters for motion
+    def set_parameters(self, channel, mode=None, frequency=None, amplitude=None):
+        """ Sets parameters for motion
 
-    #     Leave parameter as None in order to leave un-changed
-    #     :param channel:
-    #     :param mode: (int) 0 corresponds to step, anything else is scan
-    #     :param freq:
-    #     :param amp:
-    #     """
+        Leave parameter as None in order to leave un-changed
+        :param channel: (int) index of channel from 0 to self.num_ch
+        :param mode: (str) default is 'step', can use 'dc' to set DC voltage
+        :param freq:
+        :param amp:
+        """
 
-    #     if mode is not None:
-    #         result_mode = self._nanopositionersdll.SA_CTL_SetProperty_i32(
-    #             self.dhandle,
-
-    #         )
+        if mode is not None:
+            if mode == 'dc':
+                result_mode = self._nanopositionersdll.SA_CTL_SetProperty_i32(
+                    self.dhandle,
+                    channel,
+                    self.PKEY_MOVE_MODE,
+                    self.MOVE_MODE_DC
+                )
+                if result_mode:
+                    self.log.warn(
+                        f'Failed to set mode to DC for positioner {self.dev_name},'
+                        f'channel {channel}'
+                    )
+            else:
+                result_mode = self._nanopositionersdll.SA_CTL_SetProperty_i32(
+                    self.dhandle,
+                    channel,
+                    self.PKEY_MOVE_MODE,
+                    self.MOVE_MODE_STEP
+                )
+                if result_mode:
+                    self.log.warn(
+                        f'Failed to set mode to step for positioner {self.dev_name},'
+                        f'channel {channel}'
+                    )
 
     # Technical methods
 
@@ -178,6 +200,7 @@ class Nanopositioners():
 
 def main():
     nanopos = Nanopositioners()
+    nanopos.set_parameters(channel=0, mode='step')
     nanopos.close()
 
 if __name__ == "__main__":

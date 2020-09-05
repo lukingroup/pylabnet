f0260277d3c5c3b7a70758baf10312e9e3845caa
Roger <can.knaut@gmail.com>
2020-09-05 15:43:55 -0400

    rewrote counter to accomodate multiple measurements

--- pylabnet/hardware/counter/swabian_instruments/cnt_monitor.py
+++ pylabnet/hardware/counter/swabian_instruments/cnt_monitor.py
@@ -8,7 +8,7 @@ from pylabnet.utils.logging.logger import LogHandler
 
 class Wrap:
 
-    def __init__(self, tagger, ch_list=[1], logger=None):
+    def __init__(self, tagger, logger=None):
         """Instantiate count monitor
 
         :param tagger: instance of TimeTagger class
@@ -32,61 +32,88 @@ class Wrap:
         )
 
         # Counter instance
-        self._ctr = None
+        self._ctr = {}
 
         # Set click channels
-        self._ch_list = ch_list
-        self.set_ch_assignment(ch_list=ch_list)
+        self._ch_list = {}
 
-    def start_ctr(self, bin_width=1000000000, n_bins=10000):
+    def start_ctr(self, name=None, bin_width=1000000000, n_bins=10000):
         """Start counter
 
+        :param name: (str) identifier for the counter measurement
         :param bin_width: integer in ps for width of count bins
         :param n_bins: integer number of bins to store before
                             wrapping around
         """
-        
+
+        # If a name is not provided just use the index of the measurement
+        if name is None:
+            name = str(len(self._ctr))
+
         # Instantiate Counter instance, see TT documentation
-        self._ctr = TT.Counter(
-            self._tagger, 
-            channels=self._ch_list, 
-            binwidth=bin_width, 
+        self._ctr[name] = TT.Counter(
+            self._tagger,
+            channels=self._ch_list,
+            binwidth=bin_width,
             n_values=n_bins
         )
 
-    def clear_ctr(self):
+    def clear_ctr(self, name=None):
         """Resets the array to zero and restarts the measurement.
             See the clear() method of Counter class in TT
+
+        :param name: (str) identifier for the counter measurement
         """
+
+        name = self.handle_name(name)
 
         # Clear counter (see TT documentation)
-        self._ctr.clear()
+        self._ctr[name].clear()
 
-    def get_counts(self):
-        """Gets a 2D array of counts on all channels. See the 
+    def get_counts(self, name=None):
+        """Gets a 2D array of counts on all channels. See the
             getData() method of Counter class in TT
+
+        :param name: (str) identifier for the counter measurement
         """
+
+        name = self.handle_name(name)
 
         # Get count data (see TT documentation)
-        return self._ctr.getData()
+        return self._ctr[name].getData()
 
-    def get_x_axis(self):
+    def get_x_axis(self, name=None):
         """Gets the x axis in picoseconds for the count array.
             See the getIndex() method of Counter class in TT
+
+        :param name: (str) identifier for the counter measurement
         """
 
-        # Get x axis
-        return self._ctr.getIndex()
+        name = self.handle_name(name)
 
-    def set_ch_assignment(self, ch_list=[1]):
+        # Get x axis
+        return self._ctr[name].getIndex()
+
+    def set_ch_assignment(self, name=None, ch_list=[1]):
         """Sets the ch_list attribute of the wrapper to a valid
-            list of channel numbers as desired by TT, also 
+            list of channel numbers as desired by TT, also
             configures the default naming convention for channels
 
+        :param name: (str) identifier for the counter measurement
         :param ch_list: list of integer numbers of channels,
                         following the convention 1 ... 8
                         for rising edge and negative for falling
         """
 
+        if name is None:
+            name = str(len(self._ctr))
+
         # Set attribute to validated ch_list
-        self._ch_list = ch_list
+        self._ch_list[name] = ch_list
+
+    @staticmethod
+    def handle_name(name):
+        if name is None:
+            return '0'
+        else:
+            return name
--- pylabnet/network/client_server/si_tt_cnt_monitor.py
+++ pylabnet/network/client_server/si_tt_cnt_monitor.py
@@ -6,50 +6,54 @@ from pylabnet.network.core.client_base import ClientBase
 
 class Service(ServiceBase):
 
-    def exposed_start_counting(self, bin_width=1000000000, n_bins=10000):
+    def exposed_start_counting(self, name, bin_width=1000000000, n_bins=10000):
         return self._module.start_ctr(
+            name=name,
             bin_width=bin_width,
             n_bins=n_bins
         )
 
-    def exposed_clear_counter(self):
-        return self._module.clear_ctr()
+    def exposed_clear_counter(self, name):
+        return self._module.clear_ctr(name=name)
 
-    def exposed_get_counts(self):
-        res_pickle = self._module.get_counts()
+    def exposed_get_counts(self, name):
+        res_pickle = self._module.get_counts(name=name)
         return pickle.dumps(res_pickle)
 
-    def exposed_get_x_axis(self):
-        res_pickle = self._module.get_x_axis()
+    def exposed_get_x_axis(self, name):
+        res_pickle = self._module.get_x_axis(name=name)
         return pickle.dumps(res_pickle)
 
-    def exposed_set_channels(self, ch_list=[1]):
+    def exposed_set_channels(self, name, ch_list=[1]):
         return self._module.set_ch_assignment(
+            name=name,
             ch_list=ch_list
         )
 
 
 class Client(ClientBase):
 
-    def start_counting(self, bin_width=1000000000, n_bins=10000):
+    def start_counting(self, name=None, bin_width=1000000000, n_bins=10000):
         return self._service.exposed_start_counting(
+            name=name,
             bin_width=bin_width,
             n_bins=n_bins
         )
 
-    def clear_counter(self):
-        return self._service.exposed_clear_counter()
+    def clear_counter(self, name=None):
+        return self._service.exposed_clear_counter(name=name)
 
-    def get_counts(self):
-        res_pickle = self._service.exposed_get_counts()
+    def get_counts(self, name=None):
+        res_pickle = self._service.exposed_get_counts(name=name)
         return pickle.loads(res_pickle)
 
-    def get_x_axis(self):
-        res_pickle = self._service.exposed_get_x_axis()
+    def get_x_axis(self, name=None):
+        res_pickle = self._service.exposed_get_x_axis(name=name)
         return pickle.loads(res_pickle)
 
-    def set_channels(self, ch_list=[1]):
+    def set_channels(self, name=None, ch_list=[1]):
         return self._service.exposed_set_channels(
+            name=name,
             ch_list=ch_list
         )
 

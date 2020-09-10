c8bff4bbf82da812c9c4659fa7dfccb2a55c26cb
mkbhaskar <mbhaskar@g.harvard.edu>
2020-09-10 08:01:52 -0700

    Unsuccessful implementation of aom staticline GUI

---
+++ pylabnet/launchers/servers/aom_toptica.py
@@ -0,0 +1,48 @@
+''' Configures a Staticline instance to use a NIDaqmx output'''
+
+import socket
+
+from pylabnet.hardware.awg.zi_hdawg import Driver, Sequence, AWGModule
+from pylabnet.network.client_server.staticline import Service, Client
+
+import pylabnet.hardware.staticline.staticline as staticline
+
+
+from pylabnet.network.core.generic_server import GenericServer
+
+# Parameters
+dev_id = 'dev8227'
+
+def launch(**kwargs):
+    """ Connects to a NI DAQ as staticline
+
+    :param kwargs: (dict) containing relevant kwargs
+        :logger: instance of LogClient for logging purposes
+        :port: (int) port number for the Cnt Monitor server
+    """
+
+    staticline_logger = kwargs['logger']
+
+    # Instanciate HDAWG driver.
+    hd = Driver(dev_id, logger=staticline_logger)
+
+
+    aom = staticline.Driver(
+        name='AOM',
+        logger=staticline_logger,
+        hardware_module=hd,
+        DIO_bit=30,
+    ) 
+
+    # Instantiate Server
+    # Staticline server
+    staticline_service = Service()
+    staticline_service.assign_module(module=aom)
+    staticline_service.assign_logger(logger=staticline_logger)
+    staticline_service_server = GenericServer(
+        service=staticline_service,
+        host=socket.gethostbyname(socket.gethostname()),
+        port=kwargs['port']
+    )
+
+    staticline_service_server.start()

0dd4371f61d315fabb3ed4b9c415ab555bd0582e
Mihir Bhaskar <mbhaskar@g.harvard.edu>
2020-09-11 16:39:19 -0400

    implemented client-server

--- pylabnet/network/client_server/toptica_dl_pro.py
+++ pylabnet/network/client_server/toptica_dl_pro.py
@@ -22,6 +22,76 @@ class Service(ServiceBase):
         voltage = pickle.loads(voltage)
         return self._module.set_voltage(voltage)
 
+    def exposed_current_sp(self):
+        """ Gets current setpoint
+
+        :return: (float) value of current setpoint
+        """
+
+        return self._module.current_sp()
+
+    def exposed_current_act(self):
+        """ Gets measured current
+
+        :return: (float) value of actual current
+        """
+
+        return self._module.current_sp()
+    
+    def exposed_set_current(self, current):
+        """ Sets the current to desired value
+        
+        :param current: (float) value of current to set as setpoint
+        """
+
+        current = pickle.loads(current)
+        return self._module.set_current(current)
+
+    def exposed_temp_sp(self):
+        """ Gets temperature setpoint
+
+        :return: (float) value of temperature setpoint
+        """
+
+        return self._module.temp_sp()
+
+    def exposed_temp_act(self):
+        """ Gets actual DL temp
+
+        :return: (float) value of temperature
+        """
+
+        return self._module.temp_act()
+
+    def exposed_set_temp(self, temp):
+        """ Sets the current to desired value
+        
+        :param temp: (float) value of temperature to set to in Celsius
+        """
+
+        temp = pickle.loads(temp)
+        return self._module.set_temp(temp)
+
+    def exposed_configure_scan(self, offset=65, amplitude=100):
+        """ Sets the scan parameters for piezo scanning
+
+        :param offset: (float) scan offset (center value) in volts (between 0 and 130)
+        :param amplitude: (float) scan amplitude (peak to peak) in volts
+        """
+
+        return self._module.configure_scan(offset, amplitude)
+
+    def exposed_start_scan(self):
+        """ Starts a piezo scan """
+
+        return self._module.start_scan()
+
+    def exposed_stop_scan(self):
+        """ Stops piezo scan """
+
+        return self._module.stop_scan()
+
+
 
 class Client(ClientBase):
 
@@ -50,3 +120,73 @@ class Client(ClientBase):
 
         voltage = pickle.dumps(voltages[0])
         return self._service.exposed_set_voltage(voltage)
+
+    def current_sp(self):
+        """ Gets current setpoint
+
+        :return: (float) value of current setpoint
+        """
+
+        return self._service.exposed_current_sp()
+
+    def current_act(self):
+        """ Gets measured current
+
+        :return: (float) value of actual current
+        """
+
+        return self._service.exposed_current_act()
+    
+    def set_current(self, current):
+        """ Sets the current to desired value
+        
+        :param current: (float) value of current to set as setpoint
+        """
+
+        current = pickle.dumps(current)
+        return self._service.exposed_set_current(current)
+
+    def temp_sp(self):
+        """ Gets temperature setpoint
+
+        :return: (float) value of temperature setpoint
+        """
+
+        return self._service.exposed_temp_sp()
+
+    def temp_act(self):
+        """ Gets actual DL temp
+
+        :return: (float) value of temperature
+        """
+
+        return self._service.exposed_temp_act()
+
+    def set_temp(self, temp):
+        """ Sets the current to desired value
+        
+        :param temp: (float) value of temperature to set to in Celsius
+        """
+
+        temp = pickle.dumps(temp)
+        return self._service.exposed_set_temp(temp)
+
+    def configure_scan(self, offset=65, amplitude=100):
+        """ Sets the scan parameters for piezo scanning
+
+        :param offset: (float) scan offset (center value) in volts (between 0 and 130)
+        :param amplitude: (float) scan amplitude (peak to peak) in volts
+        """
+
+        return self._service.exposed_configure_scan(offset, amplitude)
+
+    def start_scan(self):
+        """ Starts a piezo scan """
+
+        return self._service.exposed_start_scan()
+
+    def stop_scan(self):
+        """ Stops piezo scan """
+
+        return self._service.exposed_stop_scan()
+        

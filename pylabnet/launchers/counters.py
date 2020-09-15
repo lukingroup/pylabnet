d1e85a9534536b1f5e990eaf4823da187f6d24bb
Mihir Bhaskar <mbhaskar@g.harvard.edu>
2020-09-15 07:18:09 -0400

    re-implemented counter driver for clarity and added histogram functionality

--- pylabnet/launchers/counters.py
+++ pylabnet/launchers/counters.py
@@ -1,7 +1,7 @@
 """ Launches the continuous count monitor application """
 
 from pylabnet.launchers.launcher import Launcher
-from pylabnet.launchers.servers import si_tt_cnt_monitor
+from pylabnet.launchers.servers import si_tt
 from pylabnet.scripts.counter import monitor_counts
 
 
@@ -9,7 +9,7 @@ def main():
 
     launcher = Launcher(
         script=[monitor_counts],
-        server_req=[si_tt_cnt_monitor],
+        server_req=[si_tt],
         gui_req=['count_monitor'],
         params=[None]
     )

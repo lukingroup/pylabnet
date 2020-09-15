2a948182525ed19bd2b92918d50842e3f530689a
Roger <can.knaut@gmail.com>
2020-09-15 11:58:56 -0400

    Added Sasha WM control, added sawtooth notebook.

--- pylabnet/launchers/sasha_lock.py
+++ pylabnet/launchers/sasha_lock.py
@@ -1,7 +1,7 @@
 """ Launches the wavemeter monitor/control application """
 
 from pylabnet.launchers.launcher import Launcher
-from pylabnet.launchers.servers import high_finesse_ws7, toptica_dlc_pro, nidaqmx
+from pylabnet.launchers.servers import high_finesse_ws7, nidaqmx
 from pylabnet.scripts.lasers import wlm_monitor
 
 
@@ -9,10 +9,10 @@ def main():
 
     launcher = Launcher(
         script=[wlm_monitor],
-        server_req=[high_finesse_ws7, toptica_dlc_pro, nidaqmx],
+        server_req=[high_finesse_ws7, nidaqmx],
         gui_req=['wavemeter_monitor'],
         params=[None],
-        config='toptica_sasha_lock'
+        config='sasha_lock'
     )
     launcher.launch()
 

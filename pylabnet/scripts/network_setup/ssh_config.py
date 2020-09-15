d3332bb911970ce322cd6802a5c618acb939af73
Roger <can.knaut@gmail.com>
2020-09-15 18:25:25 -0400

    Started SSH config script.

--- pylabnet/scripts/network_setup/ssh_config.py
+++ pylabnet/scripts/network_setup/ssh_config.py
@@ -1,5 +1,9 @@
 from pylabnet.utils.helper_methods import unpack_launcher
 from pylabnet.utils.helper_methods import show_console, hide_console, load_config
+import paramiko
+from decouple import config
+
+LOCALHOST_PW = config('LOCALHOST_PW')
 
 
 def launch(**kwargs):
@@ -7,4 +11,26 @@ def launch(**kwargs):
 
     logger, loghost, logport, clients, guis, params = unpack_launcher(**kwargs)
 
-    config = load_config(kwargs['config'], logger=kwargs['logger'])['address']
+    hosts = load_config(kwargs['config'], logger=kwargs['logger'])['hosts']
+
+    for host in hosts:
+
+        # Initiate SSH connection
+        hostname = host['hostname']
+        host_ip = host['ip']
+
+        logger.info(f"Starting SSH connection to {hostname}@{host_ip}")
+
+        ssh = paramiko.SSHClient()
+        ssh.connect(host_ip, username=hostname, password=LOCALHOST_PW)
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("ipconfig -all")
+
+        logger.info(f"{ssh_stdout}")
+
+
+
+
+
+
+
+

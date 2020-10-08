8fa76109fe4cee37aac840537d287518a84b1e92
Can Knaut <can.knaut@gmail.com>
2020-10-07 22:14:19 -0400

    Removed ref. to abstract servers.

--- pylabnet/launchers/staticline_gui_test.py
+++ pylabnet/launchers/staticline_gui_test.py
@@ -1,15 +1,14 @@
 """ Launches the staticline GUI test"""
 
 from pylabnet.launchers.launcher import Launcher
-from pylabnet.launchers.servers import abstract, abstract2
 from pylabnet.scripts.staticlines import staticline_gui_generic
 
 
 def main():
 
     launcher = Launcher(
-        script=[staticline_gui_generic], 
-        server_req=[abstract, abstract2],
+        script=[staticline_gui_generic],
+        server_req=[None],
         gui_req=[None],
         params=[None],
         config='test_config_sl'

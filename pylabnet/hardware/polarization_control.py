91862fb2ae620d86c23559e5e9650d1732c83c0e
mkbhaskar <bob6347CC!>
2020-10-28 17:12:37 -0400

    Find devices added to driver

--- pylabnet/hardware/polarization_control.py
+++ pylabnet/hardware/polarization_control.py
@@ -2,12 +2,44 @@
 import ctypes
 
 
-
 class MPC320
+    def __init__(self, logger=None):
+        """ Instantiate Polarization controllers"""
 
- # Loads Nanopositioners DLL and define arguments and result types for c function
+        try
+# Loads polarization contorolles DLL and define arguments and result types for c function
             self._polarizationdll = ctypes.windll.LoadLibrary('Thorlabs.Motion.Control.Polarizer.dll')
             self._configure_functions()
+
+# Build list of connected device
+            testSerialNo = ctypes.create_string_buffer[16]  #the way to have a mutable buffer
+            testSerialNoSize = ctypes.c_size_t(ctypes.sizeof(testSerialNo)) #size _t gives c_ulonglong, not as in manual
+            serialNo = _wtoi(argv[1])
+            testSerialNo =  "A= %d\n, B = %s\n, " % (testSerialNoSize, serialNo)
+            print(testSerialNo)
+
+# Get device list size
+            if (self._polarizationdll.TLI_BuildDeviceList() == 0)
+                devicelist = self._polarizationdll.TLI_GetDeviceListSize()
+                n = ctypes.c_short
+                n = self._polarizationdll.TLI_GetDeviceListSize()
+
+# get MPCx20 serial numbers
+                serialNos = ctypes.c_char[100]
+                self._polarizationdll.TLI_GetDeviceListByTypeExt(serialNos, 100, 38)
+                ctypes.c_char = searchContext
+                # for sure somthing else in left side 
+                ctypes.c_char_p = "A= %d\n, B = %s\n, " % (serialNos, &searchContext)
+
+                searchContext = ctypes.c_char_p
+                p = ctypes.c_char_p 
+                p ="A= %d\n, B = %s\n, " % (serialNos, searchContext)
+                               
+#get device info from device
+                #define strcuture of DeviceInfo as in hedear file
+                TLI_DeviceInfo deviceInfo
+                self._polarizationdll.TLI_GetDeviceInfo(p, deviceInfo)
+
 
 
             serial_number = ctypes.wintypse.DWORd /  ctypes.c_char_p 
@@ -18,7 +50,7 @@ def _configure_functions(self):
 
 
              self._polarizationdll.TLI_BuildDeviceList()
-             self._polarizationdll.TLI_GetDeviceListSize()
+             self._polarizationdll.TLI_GetDeviceListSize.restype = ctype.c_short
              self._polarizationdll.TLI_MPC_GetPolParams(char const * serialNo, PolarizerParameters *polParams);
              
              self._polarizationdll.TLI_MPC_Open.argtype = ctypes.c_char_p

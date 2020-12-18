af75f52251092517a4830d6d61da3ed6be40f5d6
Mihir Bhaskar <mbhaskar@g.harvard.edu>
2020-12-18 11:16:54 -0500

    got rid of gui server based implementation

--- pylabnet/launchers/fiber_coupling_front.py
+++ pylabnet/launchers/fiber_coupling_front.py
@@ -1,11 +1,11 @@
 from pylabnet.launchers.launcher import Launcher
 from pylabnet.launchers.servers import thorlabs_pm320e_front
-from pylabnet.scripts.fiber_coupling import power_monitor_direct
+from pylabnet.scripts.fiber_coupling import power_monitor
 
 def main():
 
     launcher = Launcher(
-        script=[power_monitor_direct],
+        script=[power_monitor],
         server_req=[thorlabs_pm320e_front],
         gui_req=[None],
         params=[None],

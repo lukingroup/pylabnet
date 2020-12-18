acbf141f3afe925a8eacfe9c9a7a2b07e7512392
cknaut <can.knaut@gmail.com>
2020-12-18 11:16:35 -0500

    Double Toptica locking now works.

---
+++ pylabnet/launchers/topticas_sasha_lock.py
@@ -0,0 +1,22 @@
+""" Launches the wavemeter monitor/control application """
+
+from pylabnet.launchers.launcher import Launcher
+from pylabnet.launchers.servers import high_finesse_ws7, nidaqmx, toptica_dlc_pro
+from pylabnet.scripts.lasers import wlm_monitor
+
+
+def main():
+
+    launcher = Launcher(
+        script=[wlm_monitor],
+        server_req=[high_finesse_ws7, nidaqmx, toptica_dlc_pro],
+        gui_req=[None],
+        params=[None],
+        config='topticas+sasha_lock',
+        script_server=False
+    )
+    launcher.launch()
+
+
+if __name__ == '__main__':
+    main()

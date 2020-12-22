2396c8252c68abb236db7a413b9fdd31d6e6e243
Mihir Bhaskar <mbhaskar@g.harvard.edu>
2020-12-22 10:12:19 -0500

    rewrote find_clients method

--- pylabnet/utils/helper_methods.py
+++ pylabnet/utils/helper_methods.py
@@ -381,7 +381,7 @@ def save_metadata(log, filename=None, directory=None, date_dir=False):
         filepath += '.json'
 
     try:
-        with open(filepath, 'w') as outfile:            
+        with open(filepath, 'w') as outfile:
             json.dump(log.get_metadata(), outfile, indent=4)
     except TypeError:
         log.warn('Did not save metadata')
@@ -456,8 +456,8 @@ def get_config_directory():
     ))
 
 def load_device_config(device, config, logger=None):
-    """ Returns the device config directory 
-    
+    """ Returns the device config directory
+
     :param device: (str) name of the device folder
     :param config: (str) name of the specific device config file
     :param logger: instance of LogHandler
@@ -484,8 +484,8 @@ def load_device_config(device, config, logger=None):
     return data
 
 def load_script_config(script, config, logger=None):
-    """ Returns the script config directory 
-    
+    """ Returns the script config directory
+
     :param script: (str) name of the script folder
     :param config: (str) name of the specific script config file
     :param logger: instance of LogHandler
@@ -626,22 +626,47 @@ def find_keys(input_dict, key_name):
 
     return found
 
-def find_client(logger, client_dict, client_name):
-    """Finds client from unpacked launcher client dictionary."""
-    found_clients = find_keys(client_dict, client_name)
+def find_client(clients, settings, client_type, client_config=None):
+    """ Finds a particular client from client dictionary passed from launcher
 
+    :param clients: (dict) client dictionary
+    :param settings: (dict) configuration dictionary for script
+    :param client_type: (str) type of server (e.g. nidaqmx)
+    :param client_config: (str, optional) name of config file for specific device
+        only needed if multiple devices of the same type are used in this script
+    """
+
+    found_clients = find_keys(clients, client_type)
     num_clients = len(found_clients)
 
+    # If no matched clients, log an error
     if num_clients == 0:
-        logger.error(f'Client {client_name} could not be found.')
+        logger.error(f'Client {client_type} could not be found')
+
+    # If > 1 matched clients, try to use the device config file to match
     elif num_clients > 1:
-        logger.error(f"Multiple ({num_clients}) with name {client_name} found.")
+        if client_config is not None:
+            device_settings = load_device_config(client_type, client_config, logger)
+
+            # Search through clients using device IDs
+            found_clients = find_keys(clients, device_settings['device_id'])
+
+            # If single client, return, otherwise log error
+            num_clients = len(found_clients)
+            if num_clients == 1:
+                return found_clients[0]
+            elif num_clients == 0:
+                logger.error(f'Client ID {device_settings['device_id']} not found')
+            else:
+                logger.error(f'Multiple clients with client ID {device_settings['device_id']} found')
+
+    # If only 1 matched client, just return
     else:
         return found_clients[0]
 
 def launch_device_server(server, config, log_ip, log_port, server_port, debug=False):
     """ Launches a new device server
-    
+
     :param server: (str) name of the server. Should be the directory in which the
         relevant server config file is located, and should have a corresponding
         launch file server.py in pylabnet.launchers.servers
@@ -681,8 +706,8 @@ def launch_device_server(server, config, log_ip, log_port, server_port, debug=Fa
     return log_ip, server_port
 
 def launch_script(script, config, log_ip, log_port, debug_flag, server_debug_flag, num_clients, client_cmd):
-    """ Launches a script 
-    
+    """ Launches a script
+
     :param script: (str) name of the script. Should be the directory in which the
         relevant script config file is located
     :param config: (str) name of the config file for the script, which specifies
@@ -691,10 +716,10 @@ def launch_script(script, config, log_ip, log_port, debug_flag, server_debug_fla
     :param log_ip: (str) logger IP address
     :param log_port: (int) logger port number
     :param debug_flag: (bool) whether or not to debug the script/launcher
-    :param server_debug_flag: (bool) whether or not to debug on the 
+    :param server_debug_flag: (bool) whether or not to debug on the
         server-launching level
     :param num_clients: (int) total number of clients to the log server
-    :param client_cmd: (str) a series of commandline arguments specifying the 
+    :param client_cmd: (str) a series of commandline arguments specifying the
         client dictionary details for passing to the launcher
     """
 

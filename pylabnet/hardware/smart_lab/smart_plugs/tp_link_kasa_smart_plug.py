fc19b9fd324d42e0027c26414add41d2ba09ca09
cknaut <can.knaut@gmail.com>
2021-02-17 13:40:32 -0500

    Finished driver for HS103 Smart Plug.

--- pylabnet/hardware/smart_lab/smart_plugs/tp_link_kasa_smart_plug.py
+++ pylabnet/hardware/smart_lab/smart_plugs/tp_link_kasa_smart_plug.py
@@ -4,25 +4,23 @@ import asyncio
 from kasa import Discover
 
 
-
 class Device():
 
     def __init__(self, plug_location, logger=None):
-        """ Initiilzes connection to a Smart plug.
+        """ Initializes connection to a TP Link Kasa Smart Plug.
 
-        :location: Location of Smart plug as defined in smart plug config dict.
+        :location: Location of smart plug as defined in smart plug config dict (e.g. "Powermeter Front").
         """
 
         # Instantiate log.
         self.log = LogHandler(logger=logger)
 
+        # Store smart plug location and retrieve device id.
         self.plug_location = plug_location
-
         self.plug_config = load_config('smart_plug_config')
-
         self.plug_type, self.dev_id = self.get_device_info(plug_location)
 
-        # Discover Smart Plugs
+        # Discover Smart Plugs.
         found_devices = asyncio.run(Discover.discover())
         found_device_ids = [dev.alias for dev in found_devices.values()]
 
@@ -31,8 +29,27 @@ class Device():
         else:
             self.log.info(f"Smart Plug at location '{self.plug_location}' with device id '{self.dev_id}' discovered.")
 
+        self.plug = [dev for dev in found_devices.values() if dev.alias == self.dev_id][0]
+
+        # Run update.
+        asyncio.run(self.plug.update())
+
+    def turn_on(self):
+        """Turn switch on."""
+        asyncio.run(self.plug.turn_on())
+        self.log.info(f"Smart Plug at location '{self.plug_location}' turned on.")
+
+    def turn_off(self):
+        """Turn switch off."""
+        asyncio.run(self.plug.turn_off())
+        self.log.info(f"Smart Plug at location  '{self.plug_location}' turned off.")
+
+    def is_on(self):
+        """ Returns True is plug is on."""
+        return self.plug.is_on
 
     def get_device_info(self, plug_location):
+        """ Read config dict and find device_id matching to plug_location."""
 
         dev_id, current_plug_type = None, None
 
@@ -48,25 +65,3 @@ class Device():
             return current_plug_type, dev_id
         else:
             self.log.error(f"Could not find plug location {dev_id} in smart_plug_config.json.")
-
-
-
-
-def main():
-    from pylabnet.utils.logging.logger import LogClient
-
-    # Instantiate LogClient.
-    logger = LogClient(
-        host='192.168.50.100',
-        port=47352,
-        module_tag=f'smart_plug'
-    )
-
-    sp = Device(
-        plug_location="Powermeter Front",
-        logger=logger
-    )
-
-
-if __name__ ==  "__main__":
-    main()

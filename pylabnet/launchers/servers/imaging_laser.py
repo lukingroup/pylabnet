25648d9da590ed390e6e3741a421459df53cc774
Pieter-Jan Stas <pieterjanstas@g.harvard.edu>
2021-04-23 11:43:28 -0400

    imaging laser server

--- pylabnet/launchers/servers/imaging_laser.py
+++ pylabnet/launchers/servers/imaging_laser.py
@@ -1,7 +1,7 @@
 import socket
 
 from pylabnet.utils.helper_methods import load_device_config
-import pylabnet.hardware.spectrum_analyzer.agilent_e4405B as sa
+import pylabnet.hardware.lasers.imaging_laser as laser
 from pylabnet.network.core.generic_server import GenericServer
 from pylabnet.network.client_server.agilent_e4405B import Service
 
@@ -14,19 +14,19 @@ def launch(**kwargs):
         :port: (int) port number for the spectrum analyzer server
     """
     config = kwargs['config']
-    config = load_device_config('agilent_e4405B', config, logger=kwargs['logger'])
-    spectrum_analyzer = sa.Driver(
+    config = load_device_config('imaging_laser', config, logger=kwargs['logger'])
+    il = laser.Driver(
         gpib_address=config['device_id'],
         logger=kwargs['logger']
     )
 
     # Instantiate Server
-    sa_service = Service()
-    sa_service.assign_module(module=spectrum_analyzer)
-    sa_service.assign_logger(logger=kwargs['logger'])
-    sa_server = GenericServer(
-        service=sa_service,
+    il_service = Service()
+    il_service.assign_module(module=il)
+    il_service.assign_logger(logger=kwargs['logger'])
+    il_server = GenericServer(
+        service=il_service,
         host=socket.gethostbyname_ex(socket.gethostname())[2][0],
         port=kwargs['port']
     )
-    sa_server.start()
+    il_server.start()

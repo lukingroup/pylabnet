3b8101b2537307a96f023e474a445607d366aaab
cknaut <can.knaut@gmail.com>
2021-09-20 09:44:18 -0400

    access confluunce setting from logserver

--- pylabnet/utils/confluence_handler/confluence_handler.py
+++ pylabnet/utils/confluence_handler/confluence_handler.py
@@ -7,7 +7,8 @@ import os
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
 import sys
 from functools import partial
-
+import numpy as np
+import logging
 
 class Confluence_Handler():
     def __init__(self,  parent_wins, app,  log_client):
@@ -65,9 +66,11 @@ class Confluence_Popping_Windows(QtWidgets.QMainWindow):
         # the initial fields' info
         timestamp_date = datetime.datetime.now().strftime('%b %d %Y')
         timestamp_day = datetime.datetime.now().strftime('%b %d %Y')
-        self.space_field.setText('DEV')
+        self.space_key_field.setText('DEV')
+        self.space_name_field.setText('API Dev Test Space')
         self.page_field.setText("test-uploading graphs {}".format(timestamp_day) )
-        self.upload_space = self.space_field.text()
+        self.upload_space_key = self.space_key_field.text()
+        self.upload_space_name = self.space_name_field.text()
         self.upload_page_title = self.page_field.text()
         self.upload_setting = self.setting_field.text()
         self.upload_comment = self.comment_field.text()
@@ -84,24 +87,27 @@ class Confluence_Popping_Windows(QtWidgets.QMainWindow):
     def Update_confluence_info(self):
         # access metadata
         metadata = self.log.get_metadata()
-        self.upload_space = metadata['confluence_space']
+        self.upload_space_key = metadata['confluence_space_key']
+        self.upload_space_name = metadata['confluence_space_name']
         self.upload_page_title = metadata['confluence_page']
         
         # update display
-        self.space_field.setText(self.upload_space)
+        self.space_key_field.setText(self.upload_space_key)
+        self.space_name_field.setText(self.upload_space_name)
         self.page_field.setText(self.upload_page_title)
         return
         
 
     def Popup_Update(self):
         self.ok_button.setText("OK")
-        self.space_field.setText(self.upload_space)
+        self.space_key_field.setText(self.upload_space_key)
+        self.space_name_field.setText(self.upload_space_name)
         self.page_field.setText(self.upload_page_title)
         self.setting_field.setText(self.upload_setting)
         self.comment_field.setText(self.upload_comment)
 
         self.ok_button.setText("Ok")
-        self.setWindowTitle( self.upload_space + '/' + self.upload_page_title )
+        self.setWindowTitle( self.upload_space_key + '/' + self.upload_page_title )
         self._run_gui()
 
     def Popup_Upload(self):
@@ -112,21 +118,22 @@ class Confluence_Popping_Windows(QtWidgets.QMainWindow):
 
         # display setting
         self.ok_button.setText("Upload")
-        self.space_field.setText(self.upload_space)
+        self.space_key_field.setText(self.upload_space_key)
+        self.space_name_field.setText(self.upload_space_name)
         self.page_field.setText(self.upload_page_title)
         self.setting_field.setText(self.upload_setting)
         self.comment_field.setText(self.upload_comment)
 
         # pop out
         self._run_gui()
-        self.setWindowTitle( self.upload_space + '/' + self.upload_page_title )
+        self.setWindowTitle( self.upload_space_key + '/' + self.upload_page_title )
     
     def cancel_event(self):
         self.close()
-        
 
     def okay_event(self):
-        self.upload_space = self.space_field.text()
+        self.upload_space_key = self.space_key_field.text()
+        self.upload_space_name = self.space_name_field.text()
         self.upload_page_title = self.page_field.text()
         self.upload_setting = self.setting_field.text()
         self.upload_comment = self.comment_field.text()
@@ -207,8 +214,8 @@ class Confluence_Popping_Windows(QtWidgets.QMainWindow):
     def upload_pic(self, scrn_shot_AbsPath, scrn_shot_filename):
         ''' Upload the picture if the page exists, otherwise firtst create a new page and then upload the picture
         '''
-        if( self.confluence.page_exists(self.upload_space, self.upload_page_title) ):
-            upload_page_id = self.confluence.get_page_id(self.upload_space, self.upload_page_title)
+        if( self.confluence.page_exists(self.upload_space_key, self.upload_page_title) ):
+            upload_page_id = self.confluence.get_page_id(self.upload_space_key, self.upload_page_title)
         else:
             response = self.confluence.update_or_create(
                 parent_id= self.dev_root_id, 
@@ -288,10 +295,18 @@ class Confluence_Popping_Windows(QtWidgets.QMainWindow):
 
 class LaunchControl_Confluence_Windows(QtWidgets.QMainWindow):
     def __init__(self,  controller, app,  template= 'Confluence_info_from_LaunchControl'):
+        # param (global)
+        self.url = config('CONFLUENCE_URL')
+        self.username = config('CONFLUENCE_USERNAME')
+        self.pw = config('CONFLUENCE_PW')
+        self.userkey = config('CONFLUENCE_USERKEY')
+        self.dev_root_id = config('CONFLUENCE_DEV_root_id')
+        
         # param
         self.controller = controller
         self.app = app  # Application instance onto which to load the GUI.
         self._gui_directory = "gui_templates"
+        self.dict_name_key = {}
 
         if self.app is None:
             if get_os() == 'Windows':
@@ -304,26 +319,74 @@ class LaunchControl_Confluence_Windows(QtWidgets.QMainWindow):
         # Initialize parent class QtWidgets.QDialog
         super(LaunchControl_Confluence_Windows, self).__init__()
         
+        # confluence
+        self.confluence = Confluence(
+            url='{}/wiki'.format(self.url), # need to add 'wiki', see https://github.com/atlassian-api/atlassian-python-api/issues/252
+            username=self.username,
+            password=self.pw)
 
         # load the gui, but not show
         self._load_gui(gui_template=template, run=False)
 
         # the initial fields' info
         timestamp_day = datetime.datetime.now().strftime('%b %d %Y')
-        self.space_field.setText('DEV')
+        self.space_key_field.setText('DEV')
+        self.space_name_field.setText('API Dev Test Space')
         self.page_field.setText("test-uploading graphs {}".format(timestamp_day) )
-        self.upload_space = self.space_field.text()
+        self.upload_space_key = self.space_key_field.text()
+        self.upload_space_name = self.space_name_field.text()
         self.upload_page_title = self.page_field.text()
 
-        # Handle button pressing
+        # Handle events
+        self.space_name_field.textChanged[str].connect(self.change_space_name_event)
         self.ok_button.clicked.connect(partial(self.okay_event, True))
         self.cancel_button.clicked.connect(self.cancel_event)
 
         return
 
+
     def Popup_Update(self):
+        self.controller.log_service.logger.setLevel(logging.INFO)
+        response = self.confluence.get_all_spaces(start=0, limit=500, expand=None)['results']
+        self.controller.log_service.logger.setLevel(logging.DEBUG)
+
+        # update dictionary
+        all_space_key_list = [item["key"] for item in response]
+        all_space_name_list = [item["name"] for item in response]
+        self.dict_name_key = { }
+        for item in response:
+            self.dict_name_key[item["name"]] = item['key']
+        
+
+        names = QtWidgets.QCompleter(all_space_name_list)
+        self.space_name_field.setCompleter( names )
+        self.page_field.setReadOnly(True)
+        self.page_field.setStyleSheet("background-color: gray; color: white")
+        self.setWindowTitle( self.upload_space_key + '/' + self.upload_page_title )
+
         self._run_gui()
-        self.setWindowTitle( self.upload_space + '/' + self.upload_page_title )
+        return
+        
+    def change_space_name_event(self):
+        # Detect if valid
+        if(self.space_name_field.text() not in self.dict_name_key.keys() ):
+            return
+
+        # Valid
+        self.upload_space_name = self.space_name_field.text()
+        self.upload_space_key = self.dict_name_key[self.space_name_field.text()]
+        self.space_key_field.setText(self.upload_space_key)
+        self.page_field.setStyleSheet("background-color: black; color: white")
+        self.page_field.setReadOnly(False)
+
+        # autocomplete for pages
+        self.controller.log_service.logger.setLevel(logging.INFO)
+        response = self.confluence.get_all_pages_from_space(self.upload_space_key, start=0, limit=100, status=None, expand=None, content_type='page')
+        self.controller.log_service.logger.setLevel(logging.DEBUG)
+        all_page_name_list = [item["title"] for item in response]
+        # self.controller.log_service.logger.info(all_page_name_list)
+        names = QtWidgets.QCompleter(all_page_name_list)
+        self.page_field.setCompleter( names )
 
     def cancel_event(self):
         self.close()
@@ -331,10 +394,14 @@ class LaunchControl_Confluence_Windows(QtWidgets.QMainWindow):
         
 
     def okay_event(self, is_close=True):
-        self.upload_space = self.space_field.text()
+        self.upload_space_key = self.space_key_field.text()
+        self.upload_space_name = self.space_name_field.text()
         self.upload_page_title = self.page_field.text()
-        self.controller.log_service.metadata.update(confluence_space =  self.upload_space )
+        self.controller.log_service.metadata.update(confluence_space_key =  self.upload_space_key ) 
+        self.controller.log_service.metadata.update(confluence_space_name =  self.upload_space_name )
         self.controller.log_service.metadata.update(confluence_page =  self.upload_page_title )
+        self.controller.main_window.confluence_space.setText('Space:\n' + self.upload_space_name)
+        self.controller.main_window.confluence_page.setText('Page:\n' + self.upload_page_title)
         if(is_close is True): self.close()
         return
 

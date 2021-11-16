769532d0da6db775f692f7a613bc89570edf3a6c
Will Dey <will_dey@college.harvard.edu>
2021-11-16 07:21:57 -0600

    Remove debugging output

--- check.py
+++ check.py
@@ -11,10 +11,7 @@ from interrogate import coverage as interrogate_coverage
 
 def git(*args):
     '''Run git in a subprocess with the given arguments and return its standard output as a string.'''
-    print(args)
-    output = subprocess.run(['git', *args], stdout=subprocess.PIPE, check=True).stdout.decode('utf-8')
-    print(output)
-    return output
+    return subprocess.run(['git', *args], stdout=subprocess.PIPE, check=True).stdout.decode('utf-8')
 
 
 def check(*diff_args):

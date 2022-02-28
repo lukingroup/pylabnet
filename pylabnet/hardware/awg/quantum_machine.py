c57352b44f8ba48b90ecf03176e2698911f8fce2
cknaut <can.knaut@gmail.com>
2022-02-28 13:31:37 -0500

    debugged simulator and modified variable names for consistency

--- pylabnet/hardware/awg/quantum_machine.py
+++ pylabnet/hardware/awg/quantum_machine.py
@@ -4,15 +4,14 @@
 import numpy as np
 from pip import main
 from qm.QuantumMachinesManager import QuantumMachinesManager
-from qm.qua import *
+from qm.qua import infinite_loop_, play, program, wait
 from qm.simulate import SimulationConfig
 from pylabnet.hardware.interface.gated_ctr import GatedCtrInterface
 from pylabnet.utils.logging.logger import LogHandler
 from pylabnet.network.core.service_base import ServiceBase
 from pylabnet.network.core.client_base import ClientBase
 from pylabnet.utils.decorators.dummy_wrapper import dummy_wrap
-import requests
-import json
+
 
 class Driver:
     def __init__(self, device_name='QM', host='192.168.50.97', port=80, logger=None, dummy=False) -> None:
@@ -20,19 +19,19 @@ class Driver:
         self.log = LogHandler(logger=logger)
 
         self.qmm = QuantumMachinesManager(host=host, port='80')
-        
+
         return
 
-    def execute(self, prog, config_QM):
+    def execute(self, config_QM, prog):
         qm = self.qmm.open_qm(config_QM)
         job = qm.execute(prog)
         return job
 
-    def simulate(self, prog, config, duration=2000):
+    def simulate(self, config_QM, prog, duration=2000):
         # OPX Simulate
-        job = self.qmm.simulate(config, prog, SimulationConfig(duration=duration,
-                                                include_analog_waveforms=True,    # include analog waveform names (default True)
-                                                include_digital_waveforms=True ))   # include digital waveform  (default True))
+        job = self.qmm.simulate(config_QM, prog, SimulationConfig(duration=duration,
+                                                                  include_analog_waveforms=True,    # include analog waveform names (default True)
+                                                                  include_digital_waveforms=True))   # include digital waveform  (default True))
 
         # get DAC and digital samples
         samples = job.get_simulated_samples()
@@ -42,15 +41,14 @@ class Driver:
 if __name__ == "__main__":
     from configuration import config
     myQM = Driver()
-    
+
     # prog
     print('start...')
     with program() as prog:
         with infinite_loop_():
-            play('trigger', 'tt') 
+            play('trigger', 'tt')
             # play('X', 'e_spin')
             wait(100, 'tt')
 
     # execute
-    myQM.execute(prog, config)
-    pass
+    myQM.execute(config, prog)

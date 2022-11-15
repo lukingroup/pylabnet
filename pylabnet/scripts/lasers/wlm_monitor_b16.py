30a3b67fe2e38a922c3f3bafce54ba360b323f00
Pieter-Jan Stas <79099250+pieterjanstas@users.noreply.github.com>
2022-11-15 15:46:13 -0500

    remove extraneous try statement

--- pylabnet/scripts/lasers/wlm_monitor_b16.py
+++ pylabnet/scripts/lasers/wlm_monitor_b16.py
@@ -540,13 +540,13 @@ class Channel:
 
         # if self.sp_data already exists, keep its last value so that the
         # clear data functionality doesn't override the setpoint
-        try:
-            if len(self.sp_data) > 0:
-                self.sp_data = np.ones(display_pts) * self.sp_data[-1]
-            else:
-                self.sp_data = np.ones(display_pts) * self.data[-1]
-        except:
+        # try:
+        if len(self.sp_data) > 0:
+            self.sp_data = np.ones(display_pts) * self.sp_data[-1]
+        else:
             self.sp_data = np.ones(display_pts) * self.data[-1]
+        # except:
+        #     self.sp_data = np.ones(display_pts) * self.data[-1]
 
         self.setpoint = self.sp_data[-1]
 

93864813c64085b6d7285c45a7c6ca05c435f064
cknaut <can.knaut@gmail.com>
2023-02-13 13:38:00 -0500

    Added driver for rigol multimeter.

---
+++ pylabnet/launchers/servers/rigol_dm3058E.py
@@ -0,0 +1,20 @@
+from pylabnet.network.client_server.rigol_dm3058E import Service
+from pylabnet.hardware.multimeter.rigol_dm3058E import Driver
+from pylabnet.utils.helper_methods import GenericServer, get_ip, load_device_config
+
+ 
+def launch(**kwargs):
+
+    config_dict = load_device_config('rigol_dm3058E', kwargs['config'])
+    logger = kwargs['logger']
+    ag = Driver(config_dict['device_id'], logger)
+
+    ag_service = Service()
+    ag_service.assign_module(module=ag)
+    ag_service.assign_logger(logger=kwargs['logger'])
+    ag_server = GenericServer(
+        service=ag_service,
+        host=get_ip(),
+        port=kwargs['port']
+    )
+    ag_server.start()

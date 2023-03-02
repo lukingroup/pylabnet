b9c341b7feab1391e6687182ecea154fded90144
can.knaut@gmail.com <36173574+huanyanqi@users.noreply.github.com>
2023-03-02 18:22:31 -0500

    Additional methods, implement Tek70KHelper

--- pylabnet/network/client_server/tektronix_awg70k.py
+++ pylabnet/network/client_server/tektronix_awg70k.py
@@ -7,35 +7,20 @@ from pylabnet.network.core.client_base import ClientBase
 
 class Service(ServiceBase):
 
-    def exposed_reset(self):
-        return self._module.reset()
-
     def exposed_query(self, command):
-        return self._module.device.query(command)
+        return self._module.query(command)
 
     def exposed_write(self, command):
-        return self._module.device.write(command)
+        return self._module.write(command)
 
-    def exposed_set_output_state(self, ch, state):
-        return self._module.set_output_state(ch, state)
+    def exposed_query_binary_values(self, command, **kwargs):
+        return self._module.query_binary_values(command, **kwargs)
 
-    def exposed_get_output_state(self, ch):
-        return self._module.get_output_state(ch)
-
-    def exposed_run(self):
-        return self._module.run()
-
-    def exposed_stop(self):
-        return self._module.stop()
-
-    def exposed_get_output_state(self):
-        return self._module.get_output_state()
+    def exposed_write_binary_values(self, command, data, **kwargs):
+        return self._module.write_binary_values(command, data, **kwargs)
 
 
 class Client(ClientBase):
-
-    def reset(self):
-        return self._service.exposed_reset()
 
     def query(self, command):
         return self._service.exposed_query(command)
@@ -43,17 +28,8 @@ class Client(ClientBase):
     def write(self, command):
         return self._service.exposed_write(command)
 
-    def set_output_state(self, ch, state):
-        return self._service.exposed_set_output_state(ch, state)
+    def query_binary_values(self, command, **kwargs):
+        return self._service.exposed_query_binary_values(command, **kwargs)
 
-    def get_output_state(self, ch):
-        return self._service.exposed_get_output_state(ch)
-
-    def run(self):
-        return self._service.exposed_run()
-
-    def stop(self):
-        return self._service.exposed_stop()
-
-    def get_output_state(self):
-        return self._service.exposed_get_output_state()
+    def write_binary_values(self, command, data, **kwargs):
+        return self._service.exposed_write_binary_values(command, data, **kwargs)

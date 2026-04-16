c5f64199c371e396473f2166903e5b851b5b3cd8
pjstas <pieterjanstas@g.harvard.edu>
2026-04-16 16:09:05 -0400

    allow to flexibly plot sum of any channels

--- pylabnet/scripts/counter/monitor_counts.py
+++ pylabnet/scripts/counter/monitor_counts.py
@@ -17,19 +17,17 @@ from PyQt5 import QtWidgets
 
 class CountMonitor:
 
-    def __init__(self, ctr_client: si_tt.Client, ui='count_monitor_flex', logger_client=None, server_port=None, combined_channel=False, config=None):
+    def __init__(self, ctr_client: si_tt.Client, ui='count_monitor_flex', logger_client=None, server_port=None, config=None):
         """ Constructor for CountMonitor script
 
         :param ctr_client: instance of hardware client for counter
         :param gui_client: (optional) instance of client of desired output GUI
         :param logger_client: (obj) instance of logger client.
         :param server_port: (int) port number of script server
-        :combined_channel: (bool) If true, show additional trace with summed counts.
         """
 
         self._ctr = ctr_client
         self.log = logger_client
-        self.combined_channel = combined_channel
         self._bin_width = None
         self._n_bins = None
         self._ch_list = None
@@ -56,10 +54,7 @@ class CountMonitor:
             self.config.update({'name': f'monitor{np.random.randint(1000)}'})
 
         self.log.info(self.config)
-        if self.combined_channel:
-            num_plots = len((self.config['params'])['plot_list']) + 1 # additional plot channel that is sum of all other channels
-        else:
-            num_plots = len((self.config['params'])['plot_list'])
+        num_plots = len((self.config['params'])['plot_list'])
 
         generate_widgets(self, num_plots)
 
@@ -186,56 +181,77 @@ class CountMonitor:
             channel_num = index + 1  # label_1 is for counter 1, etc.
             ch_name = ch_names_dict.get(f"{channel_num}", None)
             if ch_name is None:
-                ch_name = ""
-                self.widgets[f'label'][index].setText(f'Counter {channel_num}')
-
-            if ch_name is not None and ch_name != "":
-                self.widgets[f'label'][index].setText(f'{ch_name} (Ch {channel_num})')
+                self.widgets[f'label'][index].setText(f'Ch {channel_num}')
             else:
-                self.widgets[f'label'][index].setText(f'Counter {channel_num}')
+                self.widgets[f'label'][index].setText(f'{ch_name} (Ch {channel_num})')
 
         # For each channel, assign to the correct plot and create a curve
-        for color, channel in enumerate(self._ch_list):
+        for plot_index, plot_entry in enumerate(self._plot_list):
 
-            # Figure out which plot to assign to
-            if self._plot_list is not None:
-                for index, channel_set in enumerate(self._plot_list):
-                    if channel in channel_set:
-                        plot_index = index
+            try:
+                plot_entry_length = len(plot_entry)
+            except TypeError:
+                plot_entry_length = 0
+
+            if plot_entry_length > 0:
+                legend_label = ""
+                for j, channel in enumerate(plot_entry):
+                    if channel not in self._ch_list:
+                        self.log.error(f'channel {channel} is in plot_list but not in ch_list!')
+                        return KeyError
                         break
 
-            # Create a curve and store the widget in our dictionary
-            self.widgets[f'curve_{channel}'] = self.widgets['graph_widget'][plot_index].plot(
-                pen=pg.mkPen(color=self.gui.COLOR_LIST[color])
-            )
-            # Get channel name if available
-            ch_name = None
-            legend_label = f'Counter {channel}'
-            if self._ch_names:
-                ch_name = self._ch_names.get(f"{channel}", None)
-                if ch_name is None or ch_name == "":
-                    ch_name = ""
-                    legend_label = f'Counter {channel}'
-                else:
-                    legend_label = f'{ch_name} (Ch {channel})'
+                    # Get channel name if available
+                    ch_name = None
 
-            self.widgets['legend_widget'][plot_index].addItem(
-                self.widgets[f'curve_{channel}'], ' - ' + legend_label)
+                    if self._ch_names:
+                        ch_name = self._ch_names.get(f"{channel}", None)
+                        if ch_name is None or ch_name == "":
+                            legend_label += f'Ch {channel} + '
+                        else:
+                            legend_label += f'{ch_name} (Ch {channel}) + '
+                    else:
+                        legend_label += f'Ch {channel} + '
+
+                legend_label = legend_label[:-3]
+
+                # Create a curve and store the widget in our dictionary
+                self.widgets[f'curve_{plot_index}'] = self.widgets['graph_widget'][plot_index].plot(
+                    pen=pg.mkPen(color=self.gui.COLOR_LIST[plot_index])
+                )
+
+                self.widgets['legend_widget'][plot_index].addItem(
+                    self.widgets[f'curve_{plot_index}'], ' - ' + legend_label)
+
+            else:
+                if plot_entry not in self._ch_list:
+                    self.log.error(f'channel {plot_entry} is in plot_list but not in ch_list!')
+                    return KeyError
+
+                # Get channel name if available
+                ch_name = None
+                if self._ch_names:
+                    ch_name = self._ch_names.get(f"{channel}", None)
+                    if ch_name is None or ch_name == "":
+                        legend_label = f'Ch {channel} + '
+                    else:
+                        legend_label = f'{ch_name} (Ch {channel}) + '
+                else:
+                    legend_label = f'Ch {channel} + '
+
+                    # Create a curve and store the widget in our dictionary
+                    self.widgets[f'curve_{plot_index}'] = self.widgets['graph_widget'][plot_index].plot(
+                        pen=pg.mkPen(color=self.gui.COLOR_LIST[plot_index])
+                    )
+
+                    self.widgets['legend_widget'][plot_index].addItem(
+                        self.widgets[f'curve_{plot_index}'], ' - ' + legend_label)
 
         # Handle button pressing
         from functools import partial
 
         for plot_index, clear_button in enumerate(self.widgets['event_button']):
             clear_button.clicked.connect(partial(lambda plot_index: self._clear_plot(plot_index), plot_index=plot_index))
-
-        if self.combined_channel:
-            self.widgets['curve_combo'] = self.widgets['graph_widget'][index + 1].plot(
-                pen=pg.mkPen(color=self.gui.COLOR_LIST[color + 1])
-            )
-            self.widgets['legend_widget'][index + 1].addItem(
-                self.widgets['curve_combo'],
-                ' - ' + 'Combined Counts'
-            )
 
     def _clear_plot(self, plot_index):
         """ Clears the curves on a particular plot
@@ -244,20 +260,10 @@ class CountMonitor:
         """
 
         # First, handle case where combined count channel is clears (very ugly).
-        if self.combined_channel and plot_index == len(self._plot_list):
-            channel = 'combo'
-            # Set the curve to constant with last point for all entries
-            self.widgets[f'curve_{channel}'].setData(
-                np.ones(self._n_bins) * self.widgets[f'curve_{channel}'].yData[-1]
-            )
-        else:
-            # Find all curves in this plot
-            for channel in self._plot_list[plot_index]:
 
-                # Set the curve to constant with last point for all entries
-                self.widgets[f'curve_{channel}'].setData(
-                    np.ones(self._n_bins) * self.widgets[f'curve_{channel}'].yData[-1]
-                )
+        # self.widgets[f'curve_{plot_index}'].setData(
+        #     np.ones(self._n_bins) * self.widgets[f'curve_{plot_index}'].yData[-1]
+        # )
 
         self._ctr.clear_ctr(name=self.config['name'])
 
@@ -268,19 +274,23 @@ class CountMonitor:
         counts = self._ctr.get_counts(name=self.config['name'])
         counts_per_sec = counts * (1e12 / self._bin_width)
 
-        summed_counts = np.sum(counts_per_sec, axis=0)
+        plot_data = np.zeros([len(self._plot_list), self._n_bins])
 
         for index, count_array in enumerate(counts_per_sec):
+
+            for plot_index, plot_entry in enumerate(self._plot_list):
+                if self._ch_list[index] in plot_entry:
+                    plot_data[plot_index] += count_array
 
             # Figure out which plot to assign to
             channel = self._ch_list[index]
 
-            # Update GUI data
-            self.widgets[f'curve_{channel}'].setData(count_array)
+            # Update GUI labels
             self.widgets[f'number_label'][channel - 1].setText(str(count_array[-1]))
 
-        if self.combined_channel:
-            self.widgets['curve_combo'].setData(summed_counts)
+        # Update GUI curves
+        for plot_index, plot_entry in enumerate(self._plot_list):
+            self.widgets[f'curve_{plot_index}'].setData(plot_data[plot_index])
 
 
 def launch(**kwargs):
@@ -295,10 +305,6 @@ def launch(**kwargs):
         logger
     )
 
-    if config['combined_channel'] == 'True':
-        combined_channel = True
-    else:
-        combined_channel = False
     # Instantiate CountMonitor
     try:
         monitor = CountMonitor(
@@ -311,7 +317,6 @@ def launch(**kwargs):
             ),
             logger_client=logger,
             server_port=kwargs['server_port'],
-            combined_channel=combined_channel,
             config=config
         )
     except KeyError:

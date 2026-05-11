1a4c90f86b7067278e6243786f5812cc56d74646
pjstas <pieterjanstas@g.harvard.edu>
2026-05-11 15:21:07 -0400

    velocity launcher

---
+++ pylabnet/launchers/servers/TLB_6700.py
@@ -0,0 +1,34 @@
+""" Implements connection and server launching of NI-daqMX card for wavemeter locking"""
+
+
+from pylabnet.hardware.lasers.TLB_6700 import Driver
+from pylabnet.network.client_server.TLB_6700 import Service, Client
+from pylabnet.network.core.generic_server import GenericServer
+from pylabnet.utils.helper_methods import get_ip, hide_console, load_device_config
+
+
+def launch(**kwargs):
+    """ Connects to Newfocus Velocity laser and launches server
+
+    :param kwargs: (dict) containing relevant kwargs
+    """
+
+    # Instantiate driver
+    velocity_logger = kwargs['logger']
+    config = load_device_config('toptica_dlc_pro', kwargs['config'], velocity_logger)
+
+    tlb = Driver(
+        device_id=config['device_id'], product_id=int(config['product_id']), logger=velocity_logger
+    )
+
+    # Instantiate server
+    tlb_service = Service()
+    tlb_service.assign_module(module=tlb)
+    tlb_service.assign_logger(logger=velocity_logger)
+    tlb_server = GenericServer(
+        service=tlb_service,
+        host=get_ip(),
+        port=kwargs['port']
+    )
+
+    tlb_server.start()
